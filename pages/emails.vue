6b2871e6a6ad783ddec624def0a8f1997bdd9136
franklindesign <manghi@gmail.com>
2018-07-16 15:20:06 -0700

    updated emails form

--- pages/emails.vue
+++ pages/emails.vue
@@ -1,66 +1,62 @@
 <template>
-<v-container grid-list-xs>
-  <div class="ma-5">
-    <v-layout>
-      <v-flex xs12 lg6 md6>
-      <div id="mc_embed_signup">
-          <form   action="//hibikisushi.us3.list-manage.com/subscribe/post?u=1d5a47e0c5045755b31f3e12b&amp;id=c463e52d9b" method="post" 
-                  id="mc-embedded-subscribe-form" 
-                  name="mc-embedded-subscribe-form" 
-                  class="validate" 
-                  target="_blank" 
-                  novalidate>
-        <div id="mc_embed_signup_scroll">
-    
-          <h2 class="cursive" >Sign-up for Hibiki Emails</h2>
-          <span>Subscribe to Hibiki Emails to receive 10% off coupons.</span> 
-    
-  
-      <div class="mc-field-group mt-4">
-        <label for="mce-EMAIL"><h4>Email Address </h4></label>
-        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-      </div>
-      <div class="mc-field-group">
-        <label for="mce-FNAME">First Name </label>
-        <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-      </div>
-      <div class="mc-field-group">
-        <label for="mce-LNAME">Last Name </label>
-        <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-      </div>
-      <div class="mc-field-group size1of2">
-        <label for="mce-MMERGE3-month">Birthday </label>
-      <div class="datefield">
-        <span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="MMERGE3[month]" id="mce-MMERGE3-month"></span> / 
-        <span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="MMERGE3[day]" id="mce-MMERGE3-day"></span> 
-        <span class="small-meta nowrap">( mm / dd )</span>
-      </div>
-  </div>	
-  <div id="mce-responses" class="clear">
-      <div class="response" id="mce-error-response" style="display:none"></div>
-      <div class="response" id="mce-success-response" style="display:none"></div>
-  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-  <div style="position: absolute; left: -5000px;"><input type="text" name="b_b7da092add9faced53d200a00_df6dfb95bd" tabindex="-1" value=""></div>
-  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+  <div class="mt-5">
+    <v-layout row wrap>
+     <v-flex xs12 lg4 md6 sm6>
+    <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
 
- 
-  </div>
-  </form>
-<p class="text-xs-center">
-  Notice: Your info is only used for Hibiki Email via Mailchimp.
-  </p>
+<div id="mc_embed_signup">
+<form action="https://hibikisushi.us3.list-manage.com/subscribe/post?u=1d5a47e0c5045755b31f3e12b&amp;id=c463e52d9b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2 class="cursive">Subscribe to Hibiki Emails</h2>
+  <p>Subscribe to Hibiki Emails to receive 10% off coupons.</p>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 </div>
+<div class="mc-field-group">
+	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-LNAME">Last Name </label>
+	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+</div>
+<div class="mc-field-group size1of2">
+	<label for="mce-MMERGE3-month">Birthday </label>
+	<div class="datefield">
+		<span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="MMERGE3[month]" id="mce-MMERGE3-month"></span> / 
+		<span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="MMERGE3[day]" id="mce-MMERGE3-day"></span> 
+		<span class="small-meta nowrap">( mm / dd )</span>
+	</div>
+</div>	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1d5a47e0c5045755b31f3e12b_c463e52d9b" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <p>Notice: Your info is only used for Hibiki Emails with Mailchimp.</p>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
 </v-flex>
 </v-layout>
-</div>
-</v-container>
+  </div>
+  
 </template>
 
 <script>
-export default {
-  scrollToTop: true
-}
+  export default {
+    
+  }
 </script>
-<style scoped>
 
+<style scoped>
+  #mc_embed_signup{color: fff; background:#1c1c1c; clear:left; font:16px Helvetica,Arial,sans-serif;}
 </style>
